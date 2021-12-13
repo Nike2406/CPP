cc477a7526c77c261cdb6145c33f94ab3215f4c5
prochell <prochell@student.21-school.ru>
2021-12-14 02:01:10 +0300

    add ex02

--- cpp_00/ex02/Account.cpp
+++ cpp_00/ex02/Account.cpp
@@ -1,27 +1,104 @@
-#include "Phonebook.hpp"
+#include "Account.hpp"
+#include <iostream>
+#include <chrono>
+#include <iomanip>
 
-int	main() {
-	Phonebook	phone;
-	std::string	str;
-	int			id;
+int	Account::_nbAccounts = 0;
+int	Account::_totalAmount = 0;
+int	Account::_totalNbDeposits = 0;
+int	Account::_totalNbWithdrawals = 0;
 
-	id = 0;
-	while (str != "EXIT")
-	{
-		phone.enterCommandsMessage();
-		std::cout << "Please, enter your madness command\n>: ";
-		std::getline(std::cin, str);
-		if (std::cin.eof()) {
-			std::cout << "You're tricky!\nTry again next time..." << std::endl;
-			break;
-		} else if (str == "ADD") {
-			id = phone.addContact(id);
-			if (id == SIZE)
-				id = 0;
-		} else if (str == "SEARCH") {
-			phone.searchContact();
-		}
+int	Account::getNbAccounts( void ) {
+	return (Account::_nbAccounts);
+}
+
+int	Account::getTotalAmount( void ) {
+	return (Account::_totalAmount);
+}
+
+int	Account::getNbDeposits( void ) {
+	return (Account::_totalNbDeposits);
+}
+
+int	Account::getNbWithdrawals( void ) {
+	return (Account::_totalNbWithdrawals);
+}
+
+void	Account::_displayTimestamp( void ) {
+	std::chrono::time_point<std::chrono::system_clock> now = std::chrono::system_clock::now();
+	std::time_t time_now = std::chrono::system_clock::to_time_t(now);
+
+	tm utc_tm = *localtime(&time_now);
+	std::cout << std::setfill('0') << "[" << (utc_tm.tm_year + 1900);
+	std::cout << std::setw(2) << utc_tm.tm_mon;
+	std::cout << std::setw(2) << utc_tm.tm_mday << "_";
+	std::cout << std::setw(2) << utc_tm.tm_hour;
+	std::cout << std::setw(2) << utc_tm.tm_min;
+	std::cout << std::setw(2) << utc_tm.tm_sec << "] ";
+}
+
+void	Account::displayAccountsInfos(void) {
+	Account::_displayTimestamp();
+	std::cout << "accounts:" << Account::_nbAccounts  << ";";
+	std::cout << "total:" << Account::_totalAmount  << ";";
+	std::cout << "deposits:" << Account::_totalNbDeposits  << ";";
+	std::cout << "withdrawals:" << Account::_totalNbWithdrawals;
+	std::cout << std::endl;
+}
+
+Account::~Account( void ) {
+	Account::_displayTimestamp();
+	this->_nbAccounts--;
+	std::cout << "index:" << this->_accountIndex << ";";
+	std::cout << "amount:" << this->_amount << ";";
+	std::cout << "closed" << std::endl;
+}
+
+void	Account::makeDeposit( int deposit ) {
+	Account::_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ";";
+	std::cout << "p_amount:" <<  this->_amount << ";";
+	std::cout << "deposit:" << deposit << ";";
+	this->_amount += deposit;
+	Account::_totalAmount += deposit;
+	std::cout << "amount:" << this->_amount << ";";
+	this->_nbDeposits++;
+	Account::_totalNbDeposits++;
+	std::cout << "nb_deposits:" << this->_nbDeposits;
+	std::cout << std::endl;
+	return ;
+}
+
+bool	Account::makeWithdrawal( int withdrawal ) {
+	Account::_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ";";
+	std::cout << "p_amount:" <<  this->_amount << ";";
+	if (this->_amount < withdrawal) {
+		std::cout << "withdrawal:refused" << std::endl;
+		return (false);
+	} else {
+		std::cout << "withdrawal:" << withdrawal << ";";
+		this->_amount -= withdrawal;
+		Account::_totalAmount -= withdrawal;
+		std::cout << "amount:" << this->_amount << ";";
+		this->_nbWithdrawals++;
+		Account::_totalNbWithdrawals++;
+		std::cout << "nb_withdrawals:" << this->_nbWithdrawals;
 	}
-	std::cout << "This progrham has been finished..." << std::endl;
-	return (0);
+	std::cout << std::endl;
+	return (true);
+}
+
+int	Account::checkAmount( void ) const {
+	return (this->_amount > 0);
+}
+
+void	Account::displayStatus( void ) const {
+	Account::_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ";";
+	std::cout << "amount:" << this->_amount << ";";
+	std::cout << "deposits:" << this->_nbDeposits << ";";
+	std::cout << "withdrawals:" << this->_nbWithdrawals;
+	std::cout << std::endl;
+	return ;
 }
