27a48b7f13318a16582a751689607ee2436fce55
prochell <prochell@student.21-school.ru>
2021-12-16 22:25:49 +0300

    add cpp_02

---
+++ cpp_02/ex00/Weapon.hpp
@@ -0,0 +1,20 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+using std::string;
+using std::cout;
+using std::endl;
+
+class Weapon {
+private:
+	string	_type;
+public:
+	Weapon(string const &type);
+	Weapon(void);
+	~Weapon(void);
+
+	string const&	getType(void);
+	void	setType(string type);
+};
+
